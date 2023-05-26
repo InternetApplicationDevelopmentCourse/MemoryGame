2f4035a9d5516506ac041d63e5ddb9c0eaac3947
Lior Hassin <86710692+liorhassin@users.noreply.github.com>
2023-05-26 15:29:57 +0300

    initial file commit

---
+++ script.js
@@ -0,0 +1,76 @@
+
+
+
+
+/**
+ * Saves player name and chosen pairs in localStorage.
+ * Hides menu screen and launches the game:
+ *          Build all page elements (Timer, Name, Button(ResetGame), Container(for cards))
+ * Initiate the timer.
+ * Link function to 'Play' button.
+ */
+function startGame(){
+    /**
+     * Add check if player name is left empty or card pairs number is not chosen.
+     * only if both received, allow the rest of the function.
+     */
+    const player_name = document.querySelector(".player-name");
+    const card_pairs = document.querySelector(".card-pairs");
+    localStorage.setItem("player_name", player_name);
+    localStorage.setItem("card_pairs", card_pairs)
+
+    removeMenu();
+    buildGameBoard();
+    startTimer();
+}
+
+/**
+ * Initiates the game counter.
+ */
+function startTimer(){
+
+}
+
+function buildGameBoard(){
+    /**
+     * Builds main container with 1 div as a row inside.
+     */
+    initiateCards();
+}
+
+function initiateCards(){
+
+}
+
+function createCard(){
+    container = document.querySelector(".row");
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("col-6");
+    newDiv.classList.add("col-sm-3");
+    newDiv.classList.add("col-md-2");
+    newDiv.classList.add("col-lg-1");
+    newDiv.innerHTML = `<div class="card" style="width: 18rem;">
+    <img src="${image}" class="card-img">
+    </div>`
+    container.appendChild(newDiv);
+}
+
+function buildMenu(){
+    /**
+     * Removes game-screen
+     * Add menu screen
+     * Design:
+     * <section class="menu-screen">...innerHTML...<section>
+     */
+    const game_screen = document.querySelector(".game-screen");
+    document.removeChild(game_screen);
+}
+
+function removeMenu(){
+    const menu_screen = document.querySelector(".menu-screen");
+    document.removeChild(menu_screen);
+}
+
+function launchFirework(){
+
+}
